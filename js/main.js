93d5553350ab0ce408c17e928c72e4cffce05aaa
Denis <denverjj@mail.ru>
2020-05-06 20:56:07 +0500

    init commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
+
+
+// day 1
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+const errorAuth = document.querySelector('.error-auth');
+
+
+let login = localStorage.getItem('gDelivey');
+// console.dir(modalAuth);
+
+function toogleModalAuth() {
+    loginInput.style.borderStyle = '';
+    loginInput.style.borderColor = '';
+    errorAuth.style.display = '';
+    modalAuth.classList.toggle('is-open');
+}
+
+
+
+
+function authorized() {
+
+    function logOut() {
+        login = null;
+        localStorage.removeItem('gDelivery');
+        buttonAuth.style.display = '';
+        userName.style.display = '';
+        buttonOut.style.display = '';
+        buttonOut.removeEventListener('click', logOut);
+        checkAuth();
+    }
+
+    console.log('Авторизован');
+
+    userName.textContent = login;
+    buttonAuth.style.display = 'none';
+    userName.style.display = 'inline';
+    buttonOut.style.display = 'block';
+
+    buttonOut.addEventListener('click', logOut);
+
+}
+
+function notAuthorized() {
+    console.log('не авторизован');
+
+    function logIn(event) {
+        event.preventDefault();
+
+        if (loginInput.value.trim()) {
+            login = loginInput.value;
+            localStorage.setItem('gDelivery', login);
+            toogleModalAuth();
+            buttonAuth.removeEventListener('click', toogleModalAuth);
+            closeAuth.removeEventListener('click', toogleModalAuth);
+            logInForm.removeEventListener('submit', logIn);
+            logInForm.reset();
+            checkAuth();
+        }
+        else {
+            errorAuth.style.display = 'block';
+            loginInput.style.borderColor = 'red';
+            loginInput.style.borderStyle = 'solid';
+        }
+    }
+
+    buttonAuth.addEventListener('click', toogleModalAuth);
+    closeAuth.addEventListener('click', toogleModalAuth);
+    logInForm.addEventListener('submit', logIn);
+
+}
+
+function checkAuth() {
+    if (login) {
+        authorized();
+    }
+    else {
+        notAuthorized();
+    }
+
+}
+
+checkAuth();
